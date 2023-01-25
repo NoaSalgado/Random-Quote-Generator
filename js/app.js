05545f4970d2de210d02cf580a2d3cd026c5db3c
Noa <noasalgadodev@gmail.com>
2023-01-25 09:39:09 +0100

    Add get quotes from the api and display it in the browser

--- js/app.js
+++ js/app.js
@@ -1,67 +1,28 @@
-const quoteContainer = document.querySelector('#quote-container');
+// DOM Elements
+const quoteCointainer = document.querySelector('#quote-cointainer');
 const quoteText = document.querySelector('#quote');
-const authorText = document.querySelector('#author');
-const twitterBtn = document.querySelector('#twitter');
+const quoteAuthor = document.querySelector('#author');
+const twitterBtn = document.querySelector('twitter');
 const newQuoteBtn = document.querySelector('#new-quote');
-const loader = document.querySelector('.loader');
 
-let apiQuotes = [];
+// Global Variables
+let quotes = [];
 
-// Show loading
-function showLoadingSpinner() {
-  loader.hidden = false;
-  quoteContainer.hidden = true;
-}
-
-// Hide loading
-function removeLoadingSpinner() {
-  quoteContainer.hidden = false;
-  loader.hidden = true;
-}
-
-// Show New Quote
-function newQuote() {
-  showLoadingSpinner();
-  // Pick a random quote from apiQuotes array
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-
-  if (!quote.author) {
-    authorText.textContent = 'Unknown';
-  } else {
-    authorText.textContent = quote.author;
-  }
-
-  if (quote.text.length > 120) {
-    quoteText.classList.add('long-quote');
-  } else {
-    quoteText.classList.remove('long-quote');
-  }
-
+// Functionality
+const showNewQuote = () => {
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
   quoteText.textContent = quote.text;
-  removeLoadingSpinner();
-}
+  quoteAuthor.textContent = quote.author;
+};
 
-// Get quotes from API
-async function getQuotesFromApi() {
-  showLoadingSpinner();
-  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+const getQuotesFromAPI = (async () => {
+  const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
   try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
+    const response = await fetch(apiURL);
+    quotes = await response.json();
+    showNewQuote();
   } catch (error) {
     console.log(error);
   }
-}
-
-// Tweet quote
-function tweetQuote() {
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-  window.open(twitterUrl, '_blank');
-}
-
-getQuotes();
-
-twitterBtn.addEventListener('click', tweetQuote);
-newQuoteBtn.addEventListener('click', newQuote);
+})();
